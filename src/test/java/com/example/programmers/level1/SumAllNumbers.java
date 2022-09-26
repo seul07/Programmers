a5b823938476ecee16e097497dda0882523857d7
yeseul <sseul0701@argonet.co.kr>
2022-09-26 09:40:21 +0900

    commit

---
+++ src/test/java/com/example/programmers/level1/SumAllNumbers.java
@@ -0,0 +1,18 @@
+package com.example.programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SumAllNumbers {
+    int n = 987;
+    @Test
+    public void test() {
+        int sum = 0;
+        while(n>0){
+            sum += n%10;
+            n /= 10;
+        }
+        assertThat(sum).isEqualTo(24);
+    }
+}
