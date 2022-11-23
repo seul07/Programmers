9a3a895ecf6860644f80f45e7b30a00c671561cd
yeseul <sseul0701@argonet.co.kr>
2022-11-23 09:47:19 +0900

    commit

---
+++ src/test/java/com/example/programmers/level0/컨트롤제트.java
@@ -0,0 +1,31 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 컨트롤제트 {
+    String s = "1 1 Z Z";
+    @Test
+    public void test() {
+        String[] arr = s.split(" ");
+        int answer = 0;
+
+        Stack<String> stack = new Stack<String>();
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i].equals("Z") ){
+                stack.pop();
+            } else {
+                stack.push(arr[i]);
+            }
+        }
+        while(!stack.empty()) {
+            answer += Integer.parseInt(stack.pop());
+        }
+        System.out.println(answer);
+        assertThat(true);
+    }
+}
