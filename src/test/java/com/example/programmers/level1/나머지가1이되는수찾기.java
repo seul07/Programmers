5520cedd50640b2c6942fde9dae41c971a4f32f5
Jo Yeseul <sseul0701@argonet.co.kr>
2023-02-15 11:46:27 +0900

    level 1 나머지가 1이되는 수 찾기

---
+++ src/test/java/com/example/programmers/level1/나머지가1이되는수찾기.java
@@ -0,0 +1,21 @@
+package com.example.programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 나머지가1이되는수찾기 {
+    int n = 11;
+    @Test
+    public void test() {
+        int min = Integer.MAX_VALUE;
+        for (int i = n; i >= 2; i--) {
+            if(n%i==1){
+                min = Math.min(min,i);
+            }
+        }
+        System.out.println(min);
+        assertThat(true);
+    }
+
+}
