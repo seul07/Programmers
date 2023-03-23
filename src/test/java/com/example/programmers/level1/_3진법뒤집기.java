1228c10465a7a1efec4f33163ed11f6a127f0d4a
Jo Yeseul <sseul0701@argonet.co.kr>
2023-03-23 10:25:29 +0900

    level 1 3진법뒤집기

---
+++ src/test/java/com/example/programmers/level1/_3진법뒤집기.java
@@ -0,0 +1,18 @@
+package com.example.programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class _3진법뒤집기 {
+    int n = 125;
+    @Test
+    public void test() {
+        String three = Integer.toString(n,3);
+        StringBuffer sb = new StringBuffer(three);
+        String reversedStr = sb.reverse().toString();
+        int result = Integer.parseInt(reversedStr,3);
+
+        assertThat(result).isEqualTo(229);
+    }
+}
