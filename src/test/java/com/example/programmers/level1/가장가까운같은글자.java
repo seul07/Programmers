40061187caf413681e112d8b33067232697a7d48
Jo Yeseul <sseul0701@argonet.co.kr>
2023-04-17 10:54:36 +0900

    level 1 가장가까운같은글자, 푸드파이트대회

---
+++ src/test/java/com/example/programmers/level1/가장가까운같은글자.java
@@ -0,0 +1,23 @@
+package com.example.programmers.level1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 가장가까운같은글자 {
+    String s = "banana";
+    @Test
+    public void test() {
+        int[] arr = new int[s.length()];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+
+            }
+        }
+        assertThat(true);
+    }
+}
