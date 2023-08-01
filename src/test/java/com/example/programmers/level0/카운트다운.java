3f23ed820ef1dcda83760c8ecc1083a79f9a727a
sseul <sseul0701@argonet.co.kr>
2023-08-01 10:03:17 +0900

    레벨 0 문제풀이

---
+++ src/test/java/com/example/programmers/level0/카운트다운.java
@@ -0,0 +1,23 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 카운트다운 {
+    int start = 10;
+    int end = 3;
+    @Test
+    public void test() {
+        int[] answer = new int[start-end+1];
+        int cnt = 0;
+        for (int i = start; i >= end; i--) {
+            answer[cnt] = i;
+            cnt++;
+        }
+        System.out.println(Arrays.toString(answer));
+        assertThat(true);
+    }
+}
