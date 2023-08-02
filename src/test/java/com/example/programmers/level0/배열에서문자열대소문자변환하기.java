a8c84967333a6153c8423b117d58491a021f9a6b
sseul <sseul0701@argonet.co.kr>
2023-08-02 10:14:05 +0900

    레벨 0 문제풀이

---
+++ src/test/java/com/example/programmers/level0/배열에서문자열대소문자변환하기.java
@@ -0,0 +1,23 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 배열에서문자열대소문자변환하기 {
+    String[] strArr = {"AAA","BBB","CCC","DDD"};
+    @Test
+    public void test() {
+        String[] answer = strArr;
+        for (int i = 0; i < strArr.length; i++) {
+            if(i%2==0){
+                answer[i] = strArr[i].toLowerCase();
+            }else{
+                answer[i] = strArr[i].toUpperCase();
+            }
+        }
+        assertThat(true);
+    }
+}
