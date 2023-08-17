bc1fd69f60f589e0a75bfef612a64c92cda61de9
sseul <sseul0701@gmail.com>
2023-08-17 15:09:16 +0900

    테스트 커밋3

--- src/test/java/com/example/programmers/테스트파일.java
+++ src/test/java/com/example/programmers/테스트파일.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class 테스트파일 {
     @Test
     public void test() {
+        System.out.println("merge test");
         assertThat(true);
     }
 }
