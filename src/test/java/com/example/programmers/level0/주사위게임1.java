6ce478b56fea44d85c1523a65056b8a1a9dbbfd3
sseul <sseul0701@gmail.com>
2023-08-18 13:16:30 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/주사위게임1.java
@@ -0,0 +1,25 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 주사위게임1 {
+    int a = 3;
+    int b = 5;
+    @Test
+    public void test() {
+        int answer = 0;
+        if(a%2!=0 && b%2!=0){
+            answer = (a*a)+ (b*b);
+        }else if( (a%2==0 && b%2!=0) || (a%2!=0 && b%2==0) ){
+            answer= 2*(a+b);
+        }else if(a%2==0 && b%2==0){
+            answer = a-b;
+        }
+        if(answer<0){
+            answer = (-1)*answer;
+        }
+        assertThat(answer).isEqualTo(34);
+    }
+}
