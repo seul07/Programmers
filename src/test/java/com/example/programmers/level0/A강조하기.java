e11763c56d5f7fcc7c50dc0bbc49106ec309ef89
sseul <sseul0701@gmail.com>
2023-08-24 09:56:36 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/A강조하기.java
@@ -0,0 +1,15 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class A강조하기 {
+    String myString = "abstract algebra";
+    @Test
+    public void test() {
+        myString = myString.toLowerCase();
+        myString = myString.replaceAll("a","A");
+        assertThat(true);
+    }
+}
