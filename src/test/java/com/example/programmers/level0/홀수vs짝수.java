9b35b92143c804faefff4f199c96a4e08d543314
sseul <sseul0701@gmail.com>
2023-08-29 10:21:01 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/홀수vs짝수.java
@@ -0,0 +1,26 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 홀수vs짝수 {
+   int[] num_list = {4,2,6,1,7,6};
+    @Test
+    public void test() {
+        int answer = 0;
+        int odd=0;
+        int eve=0;
+        for (int i = 0; i < num_list.length; i++) {
+            if(i%2==0){
+                eve += num_list[i];
+            }else{
+                odd += num_list[i];
+            }
+        }
+        if(eve>odd) answer = eve;
+        else if(eve<odd) answer = odd;
+        else answer = eve;
+        assertThat(answer).isEqualTo(17);
+    }
+}
