3247a157092f47d6616334f13c6c1e81d2620246
sseul <sseul0701@gmail.com>
2023-08-31 10:25:41 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/_0떼기.java
@@ -0,0 +1,24 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class _0떼기 {
+    String n_str = "0010";
+    @Test
+    public void test() {
+        String[] arr = n_str.split("");
+        if(n_str.startsWith("0")){
+            int cnt = 0;
+            while(arr[cnt].equals("0")){
+                arr[cnt] = "";
+                cnt++;
+            }
+        }
+        String answer = String.join("",arr);
+        assertThat(true);
+    }
+}
