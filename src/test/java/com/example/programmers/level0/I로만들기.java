580a079cb9052842d61f2d0c392c33f37e9782f0
sseul <sseul0701@gmail.com>
2023-09-05 10:24:55 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/I로만들기.java
@@ -0,0 +1,27 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class I로만들기 {
+    String myString = "abcdevwxyz";
+    @Test
+    public void test() {
+        String answer = myString.replace("a", "l")
+                .replace("b", "l")
+                .replace("c", "l")
+                .replace("d", "l")
+                .replace("e", "l")
+                .replace("f", "l")
+                .replace("g", "l")
+                .replace("h", "l")
+                .replace("i", "l")
+                .replace("j", "l")
+                .replace("k", "l");
+        System.out.println(answer);
+        assertThat(true);
+    }
+}
