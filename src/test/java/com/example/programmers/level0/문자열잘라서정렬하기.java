0cd1ed77486aa5288e06b78a041d5417c9a822b2
sseul <sseul0701@gmail.com>
2023-09-13 10:40:29 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/문자열잘라서정렬하기.java
@@ -0,0 +1,28 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 문자열잘라서정렬하기 {
+    String myString = "axx";
+    @Test
+    public void test() {
+        String[] arr = myString.split("x");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(!arr[i].equals("")){
+                list.add(arr[i]);
+            }
+        }
+        String[] answer = new String[list.size()];
+        list.toArray(answer);
+        Arrays.sort(answer);
+        assertThat(true);
+    }
+}
