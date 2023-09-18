057cc6628d9fc22d0fa10b811f06d8157d949acf
sseul <sseul0701@gmail.com>
2023-09-18 10:10:04 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/간단한식계산하기.java
@@ -0,0 +1,23 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 간단한식계산하기 {
+    String binomial = "43 + 12";
+    @Test
+    public void test() {
+        String[] arr = binomial.split(" ");
+        int answer = 0;
+        if("+".equals(arr[1])){
+            answer = Integer.parseInt(arr[0]) +Integer.parseInt(arr[2]);
+        }else if("-".equals(arr[1])){
+            answer = Integer.parseInt(arr[0]) -Integer.parseInt(arr[2]);
+        }else if("*".equals(arr[1])){
+            answer = Integer.parseInt(arr[0]) *Integer.parseInt(arr[2]);
+        }
+        System.out.println(answer);
+        assertThat(true);
+    }
+}
