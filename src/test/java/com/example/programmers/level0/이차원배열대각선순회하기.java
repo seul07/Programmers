ad744b3d18d5bd61b5232d8acba6391bdfa2ea89
sseul <sseul0701@gmail.com>
2023-09-25 16:15:19 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/이차원배열대각선순회하기.java
@@ -0,0 +1,22 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 이차원배열대각선순회하기 {
+    int[][] board = {{0,1,2},{1,2,3},{2,3,4},{3,4,5}};
+    int k = 2;
+    @Test
+    public void test() {
+        int answer = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if(i+j<=k){
+                    answer += board[i][j];
+                }
+            }
+        }
+        assertThat(true);
+    }
+}
