10731c1254f01b8a413af48fca4fe5a97ec36232
sseul <sseul0701@gmail.com>
2023-12-06 09:36:30 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/토너먼트경기수.java
@@ -0,0 +1,23 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 토너먼트경기수 {
+    int n = 7;
+    @Test
+    public void test() {
+        int answer = 0;
+        while(n>1){
+            answer += n/2;
+            if(n%2==0){
+                n = n/2;
+            }else{
+                n = (n-1)/2 +1;
+            }
+        }
+        System.out.println(answer);
+        assertThat(true);
+    }
+}
