e7b987d2f48297d69d62e0652c7286530fc6ae35
sseul <sseul0701@gmail.com>
2023-12-11 10:23:29 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/_2의영역.java
@@ -0,0 +1,45 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class _2의영역 {
+//    int[] arr = {1, 2, 1, 4, 5, 2, 9};
+    int[] arr = {1,1,1};
+//    int[] arr = {1, 2, 1, 2, 1, 10, 2, 1};
+    @Test
+    public void test() {
+        int left = 0;
+        int right = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==2){
+                left = i;
+                break;
+            }
+        }
+        for (int i = arr.length-1; i >0 ; i--) {
+            if(arr[i]==2){
+                right = i;
+                break;
+            }
+        }
+        int[] answer = new int[1];
+        if(left==0 && right==arr.length){
+            answer[0] = -1;
+        }else{
+            answer = new int[right-left+1];
+            int cnt = 0;
+            for (int i = left; i < right+1; i++) {
+                answer[cnt] = arr[i];
+                cnt++;
+            }
+        }
+        assertThat(true);
+    }
+}
