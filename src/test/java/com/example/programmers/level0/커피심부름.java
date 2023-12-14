24ba9ab4e8fdc9b70d956ec3ff6b601545b8352c
sseul <sseul0701@gmail.com>
2023-12-14 09:53:56 +0900

    level 0

---
+++ src/test/java/com/example/programmers/level0/커피심부름.java
@@ -0,0 +1,24 @@
+package com.example.programmers.level0;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 커피심부름 {
+    String[] order = {"cafelatte", "americanoice", "hotcafelatte", "anything"};
+    @Test
+    public void test() {
+        int answer = 0;
+        for (int i = 0; i < order.length; i++) {
+            if(order[i].contains("cafelatte")){
+                answer += 5000;
+            }else{
+                answer += 4500;
+            }
+        }
+        assertThat(answer).isEqualTo(19000);
+    }
+}
